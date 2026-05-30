ef6efcddf0afa10e82d1753945cbf54fdae507ee
abdulloh8085 <abdulloh.im4@gmail.com>
2026-05-30 19:18:48 +0900

    f : modify routing system and controllers

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,22 @@
+
 import express from 'express';
-import path from "path";
+import path from 'path';
+import router from './router';
+import routerAdmin from "./routerAdmin";
 
-const app = express(); 
-app.use(express.static(path.join(__dirname, "public")));
-// app.use(express.static(path.join()));
-app.use(express.urlencoded({extended: true}));
+const app = express();
+app.use(express.static(path.join(__dirname, 'public')))
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-app.set('views', path.join(__dirname, "views"));
-app.set("view engine", "ejs");
+
+/** 2-SESSIONS **/
+
+/** 3 - VIEWS **/
+app.set('views', path.join(__dirname, "views"))
+app.set("view engine", "ejs")
+/** 4 - ROUTERS **/
+app.use("/admin", routerAdmin)
+app.use("/", router)
 
 export default app;
---
+++ src/router.ts
@@ -0,0 +1,5 @@
+import express from "express";
+import memberController from "./controllers/member.controllers";
+
+const router = express.Router();
+export default router;
