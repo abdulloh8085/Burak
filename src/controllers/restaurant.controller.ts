467d19544beed343038eebb893738d45ed9a4c82
abdulloh8085 <abdulloh.im4@gmail.com>
2026-06-04 19:57:15 +0900

    fix : modified controller

--- src/controllers/restaurant.controller.ts
+++ src/controllers/restaurant.controller.ts
@@ -47,5 +47,22 @@ restaurantController.processLogin = async (req: Request, res: Response) => {
     }
 };
 
+    restaurantController.processSignup = async (req: Request, res: Response) => {
+    try {
+        console.log("processSignup");
+        console.log("body:", req.body);
 
+        const newMember: MemberInput = req.body;
+        newMember.memberType = MemberType.RESTAURANT;
+
+        const memberService = new MemberService();
+        const result = await memberService.processSignup(newMember);
+
+
+        res.send(result);
+    }   catch (err) {
+        console.log("Error, processSignup:", err);
+        res.send(err);
+    }
+};
 export default restaurantController;
