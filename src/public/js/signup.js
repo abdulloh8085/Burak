f3647a83adbb26a734488e346debfed435ed94f6
abdulloh8085 <abdulloh.im4@gmail.com>
2026-06-20 07:55:27 +0900

    Feat: signup process frontend validation

--- src/public/js/signup.js
+++ src/public/js/signup.js
@@ -1,1 +1,25 @@
 console.log("Signup frontend javascript file is running");
+
+$(function () {});
+
+function validateSignupForm() {
+    const memberNick = $(".member-nick").val();
+    const memberPhone = $(".member-phone").val();
+    const memberPassword = $(".member-password").val();
+    const confirmPassword = $(".confirm-password").val();
+
+    if(
+        memberNick == "" ||
+        memberPhone === "" ||
+         memberPassword === "" ||
+          confirmPassword === ""
+        ) {
+            alert("Please Inser all required inputs");
+            return false;
+        }
+
+        if(memberPassword !== confirmPassword) {
+            alert("Passwoed differs, please check!");
+            return false;
+        }
+};
