71a4dfadc9e3db1f73b3380a6aa3ad9ce79b8360
abdulloh8085 <abdulloh.im4@gmail.com>
2026-07-04 19:12:29 +0900

    Feat: Create Middleware methods & Develop Logout process

--- src/router.ts
+++ src/router.ts
@@ -1,12 +1,13 @@
+import express from "express";
 import memberController from "./controllers/member.controllers";
 const router = express.Router();
 
 /* Member */
 router.post("/member/login", memberController.login);
 router.post("/member/signup", memberController.signup);
+router.post(
+    "/member/logout",
+    memberController.verifyAuth,
+    memberController.logout
+);
 router.get("/member/detail", memberController.verifyAuth)
-
-/* Product */
-/* Order */
-
-export default router;
