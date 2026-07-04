e9a0619d2a2e69fd2ad160876dfa985af87b222b
abdulloh8085 <abdulloh.im4@gmail.com>
2026-07-04 19:18:48 +0900

    Fex: modified GetMEmberDetail business Logic

--- src/controllers/member.controllers.ts
+++ src/controllers/member.controllers.ts
@@ -52,14 +52,28 @@ memberController.login = async (req: Request, res: Response) => {
 memberController.logout = (req: ExtendedRequest, res: Response) => {
     try {
         console.log("logout");
-        res.cookie("accessToken", null, { maxAge: 0, httpOnly: true })
-        res.status(HttpCode.OK).json({ logout: true })
+
+        res.cookie("accessToken", null, { maxAge: 0, httpOnly: true });
+        res.status(HttpCode.OK).json({ logout: true });
+
     } catch (err) {
         console.log("Error, verifyAuth:", err);
         if (err instanceof Errors) res.status(err.code).json(err);
         else res.status(Errors.standard.code).json(Errors.standard);
     }
-}
+};
+
+memberController.getMemberDetail = async (req: ExtendedRequest, res: Response) => {
+    try {
+        console.log("getMemberDetail");
+        const result = await memberService.getMemberDetail(req.member);
+        res.status(HttpCode.OK).json(result)
+    } catch (err) {
+        console.log("Error, getMemberDetail:", err);
+        if (err instanceof Errors) res.status(err.code).json(err);
+        else res.status(Errors.standard.code).json(Errors.standard);
+    }
+};
 
 memberController.verifyAuth = async (req: ExtendedRequest, res: Response, next: NextFunction) => {
     try {
@@ -80,7 +94,6 @@ memberController.verifyAuth = async (req: ExtendedRequest, res: Response, next: 
     }
 
 }
-
 memberController.retrieveAuth = async (req: ExtendedRequest, res: Response, next: NextFunction) => {
     try {
         console.log("retrieveAuth");
--- src/router.ts
+++ src/router.ts
@@ -10,4 +10,9 @@ router.post(
     memberController.verifyAuth,
     memberController.logout
 );
-router.get("/member/detail", memberController.verifyAuth)
+router.get(
+    "/member/detail",
+    memberController.verifyAuth,
+    memberController.getMemberDeteil
+    )
+
